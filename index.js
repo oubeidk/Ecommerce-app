77bb4b6134b72c4340d3c1458c590b476aae1690
oubeidk <kefioubeid@gmail.com>
2022-11-10 17:22:02 +0100

    index.js updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 const URI = process.env.MONGODB_URL;
 mongoose.connect(
@@ -24,6 +25,7 @@ app.use(express.json());
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.listen(5000, () => {
   console.log("server is running");
